c85fb40d42fc7deacc522e893881e93e30ee7ffb
DereckAn <dereckangeles15071998@gmail.com>
2024-12-17 03:15:32 -0700

    starting nosotros

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,26 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        blueish: {
+          DEFAULT: "hsl(var(--blueish))",
+        },
+        greenish: {
+          DEFAULT: "hsl(var(--greenish))",
+        },
+        purpleish: {
+          DEFAULT: "hsl(var(--purpleish))",
+        },
+        pinkish: {
+          DEFAULT: "hsl(var(--pinkish))",
+        },
+        pinkish2: {
+          DEFAULT: "hsl(var(--pinkish2))",
+        },
+        pinkish3: {
+          DEFAULT: "hsl(var(--pinkish3))",
+        },
+      },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
         custom2: ["Butler2", "montserrat"],
