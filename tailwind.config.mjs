6fb371ae60039dd70bad38777e59eb9814e0ae36
DereckAn <dereckangeles15071998@gmail.com>
2024-12-23 17:49:32 -0700

    footre

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,30 +4,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        // blueish: {
-        //   DEFAULT: "hsl(var(--blueish))",
-        // },
-        // greenish: {
-        //   DEFAULT: "hsl(var(--greenish))",
-        // },
-        // purpleish: {
-        //   DEFAULT: "hsl(var(--purpleish))",
-        // },
-        // pinkish: {
-        //   DEFAULT: "hsl(var(--pinkish))",
-        // },
-        // pinkish2: {
-        //   DEFAULT: "hsl(var(--pinkish2))",
-        // },
-        // pinkish3: {
-        //   DEFAULT: "hsl(var(--pinkish3))",
-        // },
-        blueish:"#8289AE",
-        greenish:"#AFD6CF",
-        greenish2:"#6D9BA0",
-        purpleish:"#A686AF",
-        pinkish:"#F6DDFB",
-        pinkish2:"#EBADD2",
+        greenish: {
+          DEFAULT: "hsl(var(--greenish))",
+        },
+        purpleish: {
+          DEFAULT: "hsl(var(--purpleish))",
+        },
+        purpleishLight: {
+          DEFAULT: "hsl(var(--purpleishLight))",
+        },
+        pinkish: {
+          DEFAULT: "hsl(var(--pinkish))",
+        },
+        vainillaClaro: {
+          DEFAULT: "hsl(var(--vainilla))",
+        },
+        vainilla: {
+          DEFAULT: "hsl(var(--vainilla))",
+        },
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
